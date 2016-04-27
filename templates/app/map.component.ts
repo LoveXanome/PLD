ba5e00db662ff6b7052baee41ecae034e17a9d26
kevin <maskevin74@gmail.com>
2016-04-27 09:12:06 +0200

    squelette pour la map leaflet

---
+++ templates/app/map.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from 'angular2/core';
+import {Router, RouteParams} from 'angular2/router';
+
+
+@Component({
+    selector: 'my-map',
+    templateUrl: 'app/html/map.html'
+})
+
+export class MapComponent {
+
+
+}
