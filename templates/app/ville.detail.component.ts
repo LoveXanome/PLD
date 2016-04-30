67383ba6971122b6cd3c10e5b3ac8098d7975cd0
kevin <maskevin74@gmail.com>
2016-05-01 01:03:37 +0200

    Modification des requetes + conséquences

--- templates/app/ville.detail.component.ts
+++ templates/app/ville.detail.component.ts
@@ -47,12 +47,13 @@ export class VilleDetailComponent {
     
         this._lignesUrbaines = [];
         this._lignesNonUrbaines = [];
-
+        //this._lignes = [];
         this._mapComponent = new MapComponent();
 
         this._httpRequest = new HttpRequest(this, http);
-
-        //this._httpRequest.get('http://localhost:5000/agencies/'+ this._selectedVille.id +'/routes', this.httpLigneDetails);
+        this._httpRequest.get('http://localhost:5000/agencies/'+ this._selectedVille.id +'/routes' , this.httpLignesAgences);
+        this._httpRequest.get('http://localhost:5000/agencies/'+ this._selectedVille.id, this.httpInfoAgence);
+                
         this._lignesUrbainesChecked = false;
         this._lignesNonUrbainesChecked = false;
         this._lignesAllChecked = false;
@@ -61,7 +62,7 @@ export class VilleDetailComponent {
     }
 
     ngOnInit() {
-        this._lignes = LIGNES;
+       /* this._lignes = LIGNES;
         this._lignes[0].points = STOPS_C1;
         this._lignes[1].points = STOPS_C2;
         this._lignes[2].points = STOPS_C3;
@@ -78,7 +79,7 @@ export class VilleDetailComponent {
 
 
         //TODO à supprimer
-        this._mapComponent.initMap(48.68439, 6.18496);
+        this._mapComponent.initMap(48.68439, 6.18496);*/
 
     }
 
@@ -87,48 +88,57 @@ export class VilleDetailComponent {
                             RESULTATS HTTP
 ==============================================================================
 */
-    httpLigneDetails(_this : any, data: any)
-    {
+    httpLignesAgences(_this : any, _data : any){
+        
+        //recuperation des lignes
+        _this._lignes = _data.routes;
+       
+    }
+
+     
+    httpLigneDetails(_this : any, data: any){
         //console.debug(data);
         var idLigne = data.route.id;
-        console.debug(idLigne);
+        //console.debug(idLigne);
         for (var ligne of _this._lignes)
         {
             if(ligne.id==idLigne)
-            {              
+            {   
                 ligne.points = data.route.points;
                 //console.debug(ligne.points);
                 ligne.color = randomColor();
-
-                this._printedLignes[ligne.id] = new AffichageLigne();
-                this._printedLignes[ligne.id].isPrinted = true;
-                this._printedLignes[ligne.id].polyligne = this._mapComponent.displayLine(ligne);
+                
+                _this._printedLignes[ligne.id] = new AffichageLigne();
+                _this._printedLignes[ligne.id].isPrinted = true;
+                _this._printedLignes[ligne.id].polyligne = _this._mapComponent.displayLine(ligne);
             }
         }
+        console.debug(idLigne + " chargement terminé");
     }
+     
 
     /*
     le this. est undefined, car la function est appellé par httpRequest. Il est donc passé en paramètre
     */
-    httpLignesAgences(_this : any, _data : any) {
-        var data = _data.data;
-
+    httpInfoAgence(_this : any, _data : any) {
+        
         //recuperation du nom du réseau
-        _this._selectedVille.agency = data.agency;
+        _this._selectedVille.agency = _data.name;
+        
+        //console.debug(_data.agency.location.lat + " "+ _data.agency.location.lng);
         
          //Initialisation de la map
-        _this._mapComponent.initMap(data.location.lat, data.location.lng);
-
-        //recuperation des lignes
-        _this._lignes = data.routes; 
+        _this._mapComponent.initMap(_data.agency.location.lat, _data.agency.location.lng);   
         
         /*
             trie des lignes entre urbaines ou pas
         */
+        
+       // console.debug(_this._lignes );
         for (var ligne of _this._lignes)
         {   
-            _this._httpRequest.get('http://localhost:5000/agencies/'+ _this._selectedVille.id +'/routes/'+ligne.id , _this.getLigneDetails);
-            
+            _this._httpRequest.get('http://localhost:5000/agencies/'+ _this._selectedVille.id +'/routes/'+ligne.id , _this.httpLigneDetails);
+
             if (ligne.category == true)
                 _this._lignesUrbaines.push(ligne);
             else
@@ -249,6 +259,7 @@ export class VilleDetailComponent {
                 //La ligne n'as jamais été affiché
                 if (this._printedLignes[ligne.id] === undefined) 
                 {
+                     console.debug(ligne.name + " est selectionné  mais undifined")
                     //L'affichage sera fait par le callback de la requête
                     //this._httpRequest.get('http://localhost:5000/agencies/1/routes/102', this.httpLigneDetails);
 
@@ -259,11 +270,13 @@ export class VilleDetailComponent {
                 // la ligne à été afficher mais elle n'est pas afficher actuellement
                 else if (!this._printedLignes[ligne.id].isPrinted) 
                 {
+                    console.debug(ligne.name + " est selectionné devrait etre affiché mais l'est pas")
                     this._printedLignes[ligne.id].isPrinted = true;
                     this._mapComponent.showPolyligne(this._printedLignes[ligne.id].polyligne);
                 } 
             }
             else {
+               
                 if (this._printedLignes[ligne.id] !== undefined && this._printedLignes[ligne.id].isPrinted) 
                 {
                     this._printedLignes[ligne.id].isPrinted = false;
@@ -288,7 +301,7 @@ function randomColor ( ){
     return color;    
 }
 
-var LIGNES: Ligne[] = [
+/*var LIGNES: Ligne[] = [
     { "id": 11, "name": "C1", "category": true, "points": STOPS_C1, "color": randomColor(), 'isChecked': false},
     { "id": 12, "name": "C2", "category": false, "points": STOPS_C2, "color": randomColor(), 'isChecked': false},
     { "id": 13, "name": "C3", "category": true, "points": STOPS_C3, "color": randomColor(), 'isChecked': false},
@@ -318,4 +331,4 @@ var STOPS_C4: Arret[] = [
     { "id": 21, "name": "Gare Part-Dieu",  "location":{"lng":40.547 , "lat":-0.10}, "is_stop": true }, 
     {  "id": 22, "name": "Brotteaux", "location":{"lng": 45.544 , "lat":-0.11}, "is_stop": true },
     {  "id": 23, "name": "Charpenne", "location":{"lng": 4.455 , "lat":-0.12}, "is_stop": true } 
-];
+];*/
